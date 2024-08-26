e68facfdf64911b598b1d8f58e329028acb293f6
SOUHAIL <souhailmagrane@gmail.com>
2024-08-26 15:12:53 +0100

    create a data base by mongodb and add a conection by mongoose

--- server/index.js
+++ server/index.js
@@ -1,6 +1,21 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+// this for allow to use .env in BACK END 
+dotenv.config()
 
 const app = express()
+
+// careate a conection between database and mongoose
+mongoose.connect(process.env.MONGO)
+.then(()=> {
+    console.log("have a conection with mongodb")
+})
+.catch((err)=> {
+    console.log(err)
+})
+
 
 
 

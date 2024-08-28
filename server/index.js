26ac77de3c9ed58c9287a0acb02dd4183eb65379
SOUHAIL <souhailmagrane@gmail.com>
2024-08-28 16:46:33 +0100

    clean code

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-import UserRouter from './router/user.router.js'
 import AuthRouter from './router/auth.router.js'
 import cors from 'cors'
 

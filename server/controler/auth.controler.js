f2588306e1dcc99918ae1bb7042a1b8987af10dd
SOUHAIL <souhailmagrane@gmail.com>
2024-08-29 10:38:49 +0100

    craete a signin api route

--- server/controler/auth.controler.js
+++ server/controler/auth.controler.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
+import JWT from 'jsonwebtoken'
 
 
 
@@ -22,6 +23,29 @@ const Signup = async (req , res , next) => {
   }
 
 }
+
+
+//? function SignIn
+
+export const Signin = async (req , res , next) => {
+     const {Email , Password } = req.body
+    try{
+    //? in this case i add this code to check if the eamil is exist in the database if he exist so we get the user that have this email
+    const validation = await User.findOne({Email})
+    if(!validation) return res.status(500).send('email not exist')
+    //? compare the pass that the user he will write by the pass that i have in validation
+    const comparepass = bcryptjs.compareSync(Password , validation.Password) 
+    if(!comparepass) return res.status(500).send('pass not exist')
+    //? create a JWT token
+    const token = JWT.sign({UserID : validation._id} , process.env.JWT_TOKEN)
+    //? i destructure all information of the user but i ignore the password of user to not send him with cookies
+    const {Password :pass , ...restinfo} = validation._doc ;
+    res.cookie('access-token' , token , {httpOnly : true}).status(200).json(restinfo)
+    }
+    catch(err) {
+      next(err)
+    }
+} 
 
 
 export default Signup
--- server/router/auth.router.js
+++ server/router/auth.router.js
@@ -1,7 +1,10 @@
 import express from 'express'
 import Signup from '../controler/auth.controler.js'
+import {Signin} from '../controler/auth.controler.js'
 const router = express.Router()
 
 router.post('/signup' , Signup )  
+router.post('/signin' , Signin )  
+
 
 export default router
